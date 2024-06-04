584403e14f5a69512370e0aadf1c22b25457bffb
Pira <piravinej@gmail.com>
2024-06-05 00:42:23 +0200

    Commit

--- src/config/settings.js
+++
@@ -1,7 +0,0 @@
-const settings = {
-    showHelpButton: true,  // Change this to false to hide the help button
-    language: 'c',    // Options: 'python', 'c', 'java', 'javascript', 'html', 'php'
-  };
-  
-  export default settings;
-  
